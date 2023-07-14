db5eae16ed01a061466e70b9184e92a87441d2df
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2023-07-14 10:45:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+
+  const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+  const Contact = mongoose.model('Contact', contactSchema);
+  app.post("/contact", (req, res) => {
+    const myData =new Contact(req.body)
+    myData.save().then(()=>{
+      res.send("This item has been saved into the dataBase")
+    }).catch(()=>{
+      res.status(400).send("Sorry,The item was not saved to the dataBase")
+    });
+      // res.status(200).render("contact.pug");
+    });
+    
+}
+const port = 8000;
+// EXPRESS SPECIFIC STUFF
+app.use("/static", express.static("static")); // For serving static files
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set("view engine", "pug"); // Set the template engine as pug
+app.set("views", path.join(__dirname, "views")); // Set the views directory
+
+// ENDPOINTS
+app.get("/", (req, res) => {
+  const params = {};
+  res.status(200).render("home.pug", params);
+});
+app.get("/contact", (req, res) => {
+  const params = {};
+  res.status(200).render("contact.pug", params);
+});
+
+
+
+
+
+
+// START THE SERVER
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
+});
